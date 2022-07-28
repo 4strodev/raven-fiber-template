5c9ce3df1fdeeab5bfb5e6e9232da7ce2327fb62
4strodev <jumarurex@gmail.com>
2022-07-28 10:02:32 +0200

    Solving errors on templates files and config file

---
+++ internal/server/app.go
@@ -0,0 +1,13 @@
+package server
+
+import "github.com/gofiber/fiber/v2"
+
+var App *fiber.App
+
+func init() {
+    App = fiber.New()
+
+    App.Get("/", func(c *fiber.Ctx) error {
+        return c.SendString("Hello world")
+    })
+}
